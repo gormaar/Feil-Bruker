6fbaca1188c3428dbabcbef00f689fd2db88532a
gorm <35703118+gormaar@users.noreply.github.com>
2018-04-23 19:44:07 +0200

    Update server.go

--- Oblig3/src/Oppgave 3/server.go
+++ Oblig3/src/Oppgave 3/server.go
@@ -12,16 +12,16 @@ const qotd = "“Measuring programming progress by lines \n of code is like meas
 func main()	{
 
 	fmt.Println("Starting server..")
-
-	go TCP()
+	
+	go TCP()	//Kjører serverene synkront
 	go UDP()
 
-	for    {
+	for    {	//Evig loop
 		time.Sleep(10)
 	}
 }
 
-func errorCheck(err error)	{
+func errorCheck(err error)	{	//Errorhandler
 	if err != nil{
 		fmt.Println("Error: ", err)
 		os.Exit(0)
@@ -29,31 +29,31 @@ func errorCheck(err error)	{
 }
 
 func TCP()	{
-	listen, err := net.Listen("tcp", ":17")
+	listen, err := net.Listen("tcp", ":17")	//Deklarerer TCP-serveren med port 17 for Quote of the Day
 	errorCheck(err)
 
-	tcpConn, err := listen.Accept()
+	tcpConn, err := listen.Accept()	//Setter opp forbindelse ved Dial fra client
 	errorCheck(err)
 
 	for {
-		tcpConn.Write([]byte("Quote of the day: \n" + qotd))
+		tcpConn.Write([]byte("Quote of the day: \n" + qotd))	//Skriver Quote of the Day og avslutter forbindelsen
 		}
 	defer tcpConn.Close()
 	}
 
 func UDP()	{
-	addr, err := net.ResolveUDPAddr("udp", ":17")
+	addr, err := net.ResolveUDPAddr("udp", ":17")	//Deklarerer UDP-serveren med port 17 for Quote of the Day
 	errorCheck(err)
 
-	udpConn, err := net.ListenUDP("udp", addr)
+	udpConn, err := net.ListenUDP("udp", addr)	//Lytter etter Dials fra UDP clients
 	errorCheck(err)
 
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)			//Oppretter en buffer
 
 	for {
-		_ , addr, err := udpConn.ReadFromUDP(buffer)
-		udpConn.WriteToUDP([]byte("Quote of the day: \n" + qotd), addr)
+		_ , addr, err := udpConn.ReadFromUDP(buffer)				//Leser fra client
+		udpConn.WriteToUDP([]byte("Quote of the day: \n" + qotd), addr)		// Skriver Quote of the Day
 		errorCheck(err)
 	}
-	defer udpConn.Close()
+	defer udpConn.Close()								//Avslutter forbindelsen
 }
