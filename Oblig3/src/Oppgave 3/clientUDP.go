57f6e76f13b4494608c1c43179e62eaf55def717
gorm <35703118+gormaar@users.noreply.github.com>
2018-04-23 21:38:37 +0200

    Update clientUDP.go

--- Oblig3/src/Oppgave 3/clientUDP.go
+++ Oblig3/src/Oppgave 3/clientUDP.go
@@ -13,22 +13,23 @@ import (
 func main()	{
 	fmt.Println("Starting UDP client \n")
 
-	conn, err := net.Dial("udp", "localhost:17")
+	conn, err := net.Dial("udp", "localhost:17")	//Oppretter forbindelse med UDP-server på path "localhost:17"
 	if err != nil 	{
 		log.Fatal(err)
 	}
-	fmt.Println("Press enter to see the quote of the day")
+	fmt.Println("Press enter to see the quote of the day")	//Melding til bruker om å trykke enter for at server skal printe QOTD
+								
 
-	reader := bufio.NewReader(os.Stdin)
-	stringRead, _ := reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)			//Oppretter leser
+	stringRead, _ := reader.ReadString('\n')		
 
-	fmt.Fprintf(conn, stringRead)
+	fmt.Fprintf(conn, stringRead)				 
 
-	message, err := bufio.NewReader(conn).ReadString(')')
+	message, err := bufio.NewReader(conn).ReadString(')')	//Leser melding fra serveren frem til ")"
 		if err != nil	{
 			log.Fatal(err)
 		}
-	fmt.Println("Message from server: \n" + message)
+	fmt.Println("Message from server: \n" + message)	//Skriver melding fra server 
 
-	defer conn.Close()
+	defer conn.Close()					//Avslutter forbindelse
 }
