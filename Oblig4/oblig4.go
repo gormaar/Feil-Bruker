edb3ea6874ad18149500c82e05f9f3b6a3ba302b
gorm <35703118+gormaar@users.noreply.github.com>
2018-05-11 15:28:08 +0200

    Update oblig4.go

--- Oblig4/oblig4.go
+++ Oblig4/oblig4.go
@@ -10,19 +10,19 @@ import (
 )
 
 func main()	{
-	fmt.Println("Starting application")
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting application")		//Printer melding om at applikasjonen starter
+	http.HandleFunc("/", Handler)			//Setter path og bruker Handler
+	http.ListenAndServe(":8080", nil)		//Setter port, og handler
 
 }
 
-func errorCheck(err error)	{
+func errorCheck(err error)	{			//Funksjon for feilhåndtering
 	if err != nil	{
 		log.Fatal(err)
 	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {	//Handler som parser og executer html fil
 	path := path.Join("template", "index.html")
 	tmpl, _ := template.ParseFiles(path)
 
